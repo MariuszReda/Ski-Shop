32c878ef805e08ea8d41b2207aea55ff92a2858e
MariuszReda <69969867+MariuszReda@users.noreply.github.com>
2024-12-23 22:51:36 +0100

    Ini repo

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+        private readonly StoreContext _dbcontext;
+        public ProductsController(StoreContext context, ILogger<ProductsController> logger)
+        {
+            this._dbcontext = context;
+            _logger = logger;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _dbcontext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _dbcontext.Products.FindAsync(id);
+        }
+    }
+}
---
+++ API/Data/Migrations/20241218170104_UpdateProducts.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityinStock",
+                table: "Products",
+                newName: "QuantityInStock");
+
+            migrationBuilder.RenameColumn(
+                name: "PicutreUrl",
+                table: "Products",
+                newName: "PictureUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "Descryption",
+                table: "Products",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityinStock");
+
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "PicutreUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "Descryption");
+        }
+    }
+}
